e64c0965eeae8199212b80bb032a018254823c98
dan <dan@DPCNIX>
2019-12-03 10:39:58 -0500

    Added embedded midisetup

--- edu/moravian/schirripad/MidiSetupEmbedded.java
+++ edu/moravian/schirripad/MidiSetupEmbedded.java
@@ -1,11 +1,6 @@
 package edu.moravian.schirripad;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Hashtable;
-import java.util.Scanner;
 
 import javax.sound.midi.MidiDevice;
 import javax.sound.midi.MidiSystem;
@@ -15,7 +10,9 @@ import javax.sound.midi.Transmitter;
 public class MidiSetupEmbedded {
 
 	public void init() throws MidiUnavailableException, IOException {
-		ModReciever mr = new ModReciever(MidiSystem.getMidiDevice(outDev));
+		// TODO Specify midi output
+		// Define JUCE as midi device output
+		ModReciever mr = new ModReciever(MidiSystem.getMidiDevice(null));
 
 		MidiDevice.Info[] devs = MidiSystem.getMidiDeviceInfo();
 		Transmitter[] ts = new Transmitter[devs.length];
